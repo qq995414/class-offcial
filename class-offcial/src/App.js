96a676b68482722d563e6ea9f4b92bc871bce4bc
qq995414 <80391152+qq995414@users.noreply.github.com>
2022-11-04 13:44:45 +0800

    init project

---
+++ class-offcial/src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" />
+        <Route path="/products" />
+        <Route path="/contact" />
+        <Route path="/about" />
+      </Routes>
+    </Router>);
+}
+
+export default App;
